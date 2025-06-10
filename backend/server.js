84532ae4c54c9b779b72c75328dd6eae3771e9c6
June <yoo.junehyuk@gmail.com>
2025-06-10 11:54:05 +0800

    Set up axios for front/backend communication.
    Need to start adding routes for my webapp.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
+const cors = require("cors");
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("hello world!");
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.get("/test", (req, res) => {
+  res.send("This was sent from the server.");
 });
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
