f6a27feb9f70e98b8850015cf04d4f0f80f46eb2
June <yoo.junehyuk@gmail.com>
2025-06-14 21:59:31 +0800

    Reusing RevealOnScroll component from portfolio

--- frontend/src/routes/home.tsx
+++ frontend/src/routes/home.tsx
@@ -1,55 +1,71 @@
 // home.tsx
 import Footer from "../components/Footer";
+import RevealOnScroll from "../components/RevealOnScroll";
 import "../styles/home.css";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <video
+        className="hero-video"
+        src="/background.mp4"
+        autoPlay
+        loop
+        muted
+        playsInline
+      ></video>
+
+      <div className="hero-content">
+        <h1>Openfloor.</h1>
+        <h3>We welcome all: debaters, philosophers, and thinkers alike.</h3>
+      </div>
+    </section>
+  );
+};
+
+const Info = () => {
+  return (
+    <section className="info-section">
+      <h2>What is Openfloor?</h2>
+      <p>
+        Openfloor is a debate platform designed for students and enthusiasts to
+        engage in structured, meaningful argumentation. Whether you're new to
+        debating or a seasoned speaker, our platform lets you challenge others,
+        vote on arguments, and track your growth over time.
+      </p>
+
+      <div className="features">
+        <div className="feature-card">
+          <h3>Public & Private Debates</h3>
+          <p>
+            Engage in structured debates against other users in real-time.
+            Create a debate, choose a topic, and establish stances to support.
+          </p>
+        </div>
+        <div className="feature-card">
+          <h3>Community Voting</h3>
+          <p>
+            Let the community decide the winner based on reasoning strength.
+          </p>
+        </div>
+        <div className="feature-card">
+          <h3>Skill Progression</h3>
+          <p>Earn ranks, track stats, and build your debate resume.</p>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 const Home = () => {
   return (
     <div className="home-container">
-      <section className="hero">
-        <video
-          className="hero-video"
-          src="/background.mp4"
-          autoPlay
-          loop
-          muted
-          playsInline
-        ></video>
-
-        <div className="hero-content">
-          <h1>Openfloor.</h1>
-          <h3>We welcome all: debaters, philosophers, and thinkers alike.</h3>
-        </div>
-      </section>
-
-      <section className="info-section">
-        <h2>What is Openfloor?</h2>
-        <p>
-          Openfloor is a debate platform designed for students and enthusiasts
-          to engage in structured, meaningful argumentation. Whether you're new
-          to debating or a seasoned speaker, our platform lets you challenge
-          others, vote on arguments, and track your growth over time.
-        </p>
-
-        <div className="features">
-          <div className="feature-card">
-            <h3>Public & Private Debates</h3>
-            <p>
-              Engage in structured debates against other users in real-time.
-              Create a debate, choose a topic, and establish stances to support.
-            </p>
-          </div>
-          <div className="feature-card">
-            <h3>Community Voting</h3>
-            <p>
-              Let the community decide the winner based on reasoning strength.
-            </p>
-          </div>
-          <div className="feature-card">
-            <h3>Skill Progression</h3>
-            <p>Earn ranks, track stats, and build your debate resume.</p>
-          </div>
-        </div>
-      </section>
+      <RevealOnScroll>
+        <Hero />
+      </RevealOnScroll>
+      <RevealOnScroll>
+        <Info />
+      </RevealOnScroll>
       <Footer />
     </div>
   );
--- frontend/src/routes/register.tsx
+++ frontend/src/routes/register.tsx
@@ -1,6 +1,7 @@
 // register.tsx
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import RevealOnScroll from "../components/RevealOnScroll";
 import api from "../../api/axios";
 import axios from "axios";
 import "../styles/loginRegister.css";
@@ -51,46 +52,48 @@ const Register = () => {
         <video src="/register-background.mp4" autoPlay loop muted></video>
       </div>
       <div className="form-container">
-        <h1>You're making a great decision.</h1>
-        <form onSubmit={handleRegister} id="register-form">
-          <div className="input-container">
-            <input
-              type="email"
-              id="email"
-              className="input-line"
-              placeholder="Email*"
-              required
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="input-container">
-            <input
-              type="string"
-              id="username"
-              className="input-line"
-              placeholder="Username*"
-              required
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
-          <div className="input-container">
-            <input
-              type="password"
-              id="password"
-              className="input-line"
-              placeholder="Password*"
-              required
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <button type="submit">Register</button>
-        </form>
-        <p className="cl-account">
-          Already have an account? <a href="/register">Log in here!</a>
-        </p>
+        <RevealOnScroll>
+          <h1>You're making a great decision.</h1>
+          <form onSubmit={handleRegister} id="register-form">
+            <div className="input-container">
+              <input
+                type="email"
+                id="email"
+                className="input-line"
+                placeholder="Email*"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="input-container">
+              <input
+                type="string"
+                id="username"
+                className="input-line"
+                placeholder="Username*"
+                required
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="input-container">
+              <input
+                type="password"
+                id="password"
+                className="input-line"
+                placeholder="Password*"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button type="submit">Register</button>
+          </form>
+          <p className="cl-account">
+            Already have an account? <a href="/register">Log in here!</a>
+          </p>
+        </RevealOnScroll>
       </div>
       {errorMessage && (
         <div style={{ color: "red", marginTop: "10px" }}>{errorMessage}</div>
