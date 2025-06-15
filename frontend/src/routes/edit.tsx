034bf4d18f8318947c1a6c9d1dd2f1efeca0eaa7
June <yoo.junehyuk@gmail.com>
2025-06-15 19:15:26 +0800

    Can now edit profile from frontend

--- frontend/src/routes/edit.tsx
+++ frontend/src/routes/edit.tsx
@@ -2,17 +2,69 @@
 import React, { useState } from "react";
 import RevealOnScroll from "../components/RevealOnScroll";
 import { useAuth } from "../context/authContext";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import api from "../../api/axios";
+import axios from "axios";
 import "../styles/loginRegister.css";
 
 const Edit = () => {
-  const { user } = useAuth();
+  const { user, setUser } = useAuth();
   const [newUsername, setNewUsername] = useState<string>(user!.username);
   const [newPassword, setNewPassword] = useState<string>("");
   const [confirmNewPwd, setConfirmNewPwd] = useState<string>("");
+  const navigate = useNavigate();
 
-  const handleEdit = (e: React.FormEvent) => {
+  const handleEdit = async (e: React.FormEvent) => {
     e.preventDefault();
-    console.log("got here");
+
+    if (newPassword != confirmNewPwd) {
+      toast.error("Your passwords do not match.", {
+        position: "bottom-right",
+        theme: "dark",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+
+    try {
+      const response = await api.put("/profile/edit", {
+        username: newUsername,
+        newPassword: newPassword.trim() !== "" ? newPassword : undefined, // don't send empty password
+      });
+
+      toast.success("Your profile has been updated!", {
+        position: "bottom-right",
+        theme: "dark",
+        autoClose: 5000,
+      });
+
+      setNewPassword("");
+      setConfirmNewPwd("");
+      navigate("/profile");
+      setUser(response.data.user);
+    } catch (error) {
+      console.error("Edit failed:", error);
+      if (axios.isAxiosError(error)) {
+        const { message } = error.response!.data as {
+          message: string;
+        };
+        toast.error(message || "Failed to update profile.", {
+          position: "bottom-right",
+          theme: "dark",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      }
+    }
   };
 
   return (
@@ -30,8 +82,7 @@ const Edit = () => {
                 type="text"
                 id="edit-username"
                 className="input-line"
-                placeholder="Username*"
-                required
+                placeholder="Username"
                 value={newUsername}
                 onChange={(e) => setNewUsername(e.target.value)}
               />
@@ -41,8 +92,7 @@ const Edit = () => {
                 type="password"
                 id="edit-password"
                 className="input-line"
-                placeholder="New Password*"
-                required
+                placeholder="New Password"
                 value={newPassword}
                 onChange={(e) => setNewPassword(e.target.value)}
               />
@@ -52,8 +102,7 @@ const Edit = () => {
                 type="password"
                 id="edit-confirm-password"
                 className="input-line"
-                placeholder="Confirm New Password*"
-                required
+                placeholder="Confirm New Password"
                 value={confirmNewPwd}
                 onChange={(e) => setConfirmNewPwd(e.target.value)}
               />
@@ -61,8 +110,8 @@ const Edit = () => {
             <button type="submit">Save</button>
           </form>
           <p className="cl-account" style={{ margin: "1rem 5rem" }}>
-            Tip: You may use the same password if you only want to change your
-            username!
+            Tip: You may leave the passwords blank if you only want to change
+            your username!
           </p>
         </RevealOnScroll>
       </div>
