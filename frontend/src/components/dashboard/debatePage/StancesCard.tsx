fd92014f12bcdee4a481e17c3d7e819de9801e35
June <yoo.junehyuk@gmail.com>
2025-06-26 23:46:00 +0800

    Added basic accordion to stances card

--- frontend/src/components/dashboard/debatePage/StancesCard.tsx
+++ frontend/src/components/dashboard/debatePage/StancesCard.tsx
@@ -1,18 +1,18 @@
 // components/dashboard/debatePage/StancesCard.tsx
+import { Card, CardContent, CardHeader, CardTitle } from "../../ui/card";
 import {
-  Card,
-  CardContent,
-  //   CardDescription,
-  CardHeader,
-  CardTitle,
-} from "../../ui/card";
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "../../ui/accordion";
 import { hasAdminPermissions } from "../../../utils/debateUtils";
-import { useDebateContext } from "../../../context/debateContext";
+import { useDebateContextNonNull } from "../../../context/debateContext";
 import { Button } from "../../ui/button";
 import { SiteHeader as PageHeader } from "../site-header";
 
 const StancesCard = () => {
-  const { debate, userDetails } = useDebateContext();
+  const { debate, userDetails } = useDebateContextNonNull();
   const isAdmin = userDetails && hasAdminPermissions(userDetails.role);
 
   if (!debate) {
@@ -32,19 +32,28 @@ const StancesCard = () => {
         <CardTitle className="text-2xl font-semibold">Stances</CardTitle>
       </CardHeader>
       <CardContent className="flex flex-col gap-2">
-        {debate.stances!.length > 0 ? (
-          debate.stances!.map((stance) => {
-            return (
-              <Card
-                key={stance.id}
-                className="p-4 bg-gradient-to-t from-neutral-900 to-neutral-800 transition duration-300"
-              >
-                <h3 className="text-lg text-center font-semibold">
-                  {stance.label}
-                </h3>
-              </Card>
-            );
-          })
+        {debate.stances.length > 0 ? (
+          <Accordion type="multiple" className="w-full">
+            {debate.stances.map((stance) => {
+              return (
+                <AccordionItem key={stance.id} value={`stance-${stance.id}`}>
+                  <AccordionTrigger className="text-lg">
+                    {stance.label}
+                  </AccordionTrigger>
+                  <AccordionContent className="flex flex-col gap-4">
+                    {stance.justifications &&
+                    stance.justifications.length > 0 ? (
+                      stance.justifications.map((j) => {
+                        return <p>{j.content}</p>;
+                      })
+                    ) : (
+                      <p>No justifications yet.</p>
+                    )}
+                  </AccordionContent>
+                </AccordionItem>
+              );
+            })}
+          </Accordion>
         ) : isAdmin ? (
           <p>Add a new stance!</p>
         ) : (
