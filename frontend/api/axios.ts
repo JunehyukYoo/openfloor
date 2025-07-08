27d9b506711c70eec6fb15520dcd52a91a71c87c
June <yoo.junehyuk@gmail.com>
2025-07-08 15:16:50 +0800

    Fixing cookie issues for Safari and icognito
    Issues with cross-origin requests blocking cookies on Safari and
    icognito browers.

--- frontend/api/axios.ts
+++ frontend/api/axios.ts
@@ -3,7 +3,10 @@ import axios from "axios";
 
 // Just to cleanup fetching from backend
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? import.meta.env.VITE_API_BASE_URL
+      : "/api", // use Vercel rewrites in prod for Safari and icognito cross-origin cookie issues
   withCredentials: true,
 });
 
